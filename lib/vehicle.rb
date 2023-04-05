5199467201e765c7be3e8dfcb2cc26bd28d5a73f
Steven Stackhouse <stackmm@gmail.com>
2023-04-05 10:22:24 -0600

    add attributes and methods to Vehicle

---
+++ lib/vehicle.rb
@@ -0,0 +1,31 @@
+class Vehicle
+  attr_reader :year, :make, :model, :passengers
+
+  def initialize(year, make, model)
+    @year = year
+    @make = make
+    @model = model
+    @speeding = false
+    @passengers = []
+  end
+
+  def speeding?
+    @speeding
+  end
+
+  def speed
+    @speeding = true
+  end
+
+  def add_passenger(passenger)
+    @passengers << passenger
+  end
+
+  def num_adults
+    num_adults = 0
+    @passengers.each do |passenger|
+      num_adults += 1 if passenger.adult?
+    end
+    num_adults
+  end
+end
